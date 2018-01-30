001ba7a5c394f56757872ebe91cb7adf1f24ae3d
Kirill Osokin <Kirill_Osokin@epam.com>
2018-01-30 23:47:27 +0100

    DI

---
+++ injector.js
@@ -0,0 +1,97 @@
+'use strict';
+
+function createInjector(modulesToLoad, strictDi) {
+    var FN_ARGS = /^function\s*[^\(\s*([^\)]*)\)/m;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /\/\*.*\*\//g;
+    var cache = {};
+    var loadedModules = {};
+    strictDi = (strictDi === true);
+
+    var $provide = {
+        constant: function(key, value) {
+            if (key === 'hasOwnProperty') {
+                throw 'hasOwnProperty is not a valid constant name';
+            }
+            cache[key] = value;
+        },
+        provider: function(key, provider) {
+            cache[key] = invoke(provider.$get, provider);
+        }
+    };
+
+    /*
+     * Return a list of arguments - ['a','b'] from ['a','b', function(){}]
+     */
+    function annotate(fn) {
+        if (_.isArray(fn)) {
+            return fn.slice(0, fn.length - 1);
+        } else if (fn.$inject) {
+            return fn.$inject;
+        } else if (!fn.length) {
+            return [];
+        } else {
+            if (strictDi) {
+                throw 'fn is not using explicit annotation and ' +
+                        'cannot be invoked in strict mode';
+            }
+            var source = fn.toString().replace(STRIP_COMMENTS, '');
+            var argDeclaration = source.match(FN_ARGS);
+            return _.map(argDeclaration[1].split(','), function(argName) {
+                return argName.match(FN_ARG)[2];
+            }
+        }
+    }
+
+    /*
+    * Invokes function and injects dependencies to it
+    */
+    function invoke(fn, self, locals) {
+        var args = _.map(annotate(fn), function(token) {
+            if (_.isString(token)) {
+                return locals && locals.hasOwnProperty(token) ? locals[token] : cache[token];
+            } else {
+                return 'Incorrect injection token! Expected a string, got ' + token;
+            }
+        });
+        if (_.isArray(fn)) {
+            fn = _.last(fn);
+        }
+        return fn.apply(self, args);
+    }
+
+    /*
+    * Create new object of type Type and invokes it injecting dependencies
+    */
+    function instantiate(Type, locals) {
+        var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
+        var instance = Object.create(UnwrappedType.prototype);
+        invoke(Type, instance, locals);
+        return instance;
+    }
+
+    _.forEach(modulesToLoad, function loadModule(moduleName) {
+        if (!loadedModules.hasOwnProperty(moduleName)) {
+            loadedModules[moduleName] = true;
+            var module = angular.module(moduleName);
+            _.forEach(module.requires, loadModule);
+            _.forEach(module._invokeQueue, function(invokeArgs) {
+                var method = invokeArgs[0];
+                var args = invokeArgs[1];
+                $provide[method].apply($provide, args);
+            });
+        }
+    });
+
+    return {
+        has: function(key) {
+            return cache.hasOwnProperty(key);
+        },
+        get: function(key) {
+            return cache[key];
+        },
+        annotate: annotate,
+        invoke: invoke,
+        instantiate: instantiate
+    };
+}
---
+++ loader.js
@@ -0,0 +1,68 @@
+'use strict';
+
+function setupModuleLoader(window) {
+	/**
+	* function ensure
+	*/
+	var ensure = function(obj, name, factory) {
+		return obj[name] || (obj[name] = factory());
+	};
+
+	/**
+	* function createModule
+	*/
+	var createModule = function(name, requires, modules) {
+	    if (name === 'hasOwnProperty') {
+	        throw 'hasOwnProperty is not a valid module name';
+	    }
+	    var invokeQueue = [];
+
+	    var invokeLater = function(method) {
+	        return function() {
+	            invokeQueue.push(['method', arguments]);
+	            return moduleInstance;
+	        }
+	    }
+
+		var moduleInstance = {
+			name: name,
+			requires: requires,
+			constant: invokeLater('constant'),
+			provider: invokeLate('provider'),
+			},
+			_invokeQueue: invokeQueue
+		};
+		modules[name] = moduleInstance;
+		return moduleInstance;
+	};
+
+	/**
+	* function getModule
+	*/
+	var getModule = function(name, modules) {
+	    if (modules.hasOwnProperty(name)) {
+    	    return modules[name];
+	    } else {
+	        throw 'Module ' + name + ' is not available!';
+	    }
+	}
+
+	/**
+	* register 'angular' object
+	*/
+	var angular = ensure('angular', window, Object);
+
+    /**
+    * register 'angular.module' function
+    */
+	ensure(angular, 'module', function() {
+        var modules = {};
+        return function(name, requires) {
+            if (requires) {
+                return createModule(name, requires, modules);
+            } else {
+                getModule(name, modules);
+            }
+        }
+	});
+}
